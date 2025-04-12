3b77a545ed406511d657f848f7fa77412b378ea1
jthampiratwong <jthampiratwong@ucsb.edu>
2025-04-11 21:43:09 -0700

    update the actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Perry T.";
     }
 
     /**
@@ -30,7 +26,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "perryjirakit";
     }
 
     /**
@@ -40,13 +36,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-15");
+        team.addMember("Perry T.");
+        team.addMember("Bryce I.");
+        team.addMember("Lucas H.");
+        team.addMember("David D.");
+        team.addMember("Tyler W.");
+        team.addMember("Yue C.");
         return team;
     }
 }
